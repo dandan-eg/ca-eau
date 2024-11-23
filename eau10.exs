92886e1363b81b46a16e67b13f028c7ded5eed87
dandan-eg <egalledev@gmail.com>
2024-11-23 20:31:58 +0100

    eau10.exs OK

---
+++ eau10.exs
@@ -0,0 +1,24 @@
+# Index wanted
+defmodule Exercice do
+  def find_index(list, target) do
+    do_find_index(list, target, 0)
+  end
+
+  defp do_find_index([head | _tail], head, index), do: index
+  defp do_find_index([], _target, _index), do: -1
+
+  defp do_find_index([_head | tail], target, index) do
+    do_find_index(tail, target, index + 1)
+  end
+end
+
+case System.argv() do
+  [] ->
+    IO.puts("usage elixir eau10.exs")
+
+  args ->
+    {word_to_find, rest_of_words} = List.pop_at(args, -1)
+
+    index = Exercice.find_index(rest_of_words, word_to_find)
+    IO.puts(index)
+end
