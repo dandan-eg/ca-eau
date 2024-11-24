2bbcb8a2baa4dcc9c808e210a413bd8280e22bd4
dandan-eg <egalledev@gmail.com>
2024-11-24 23:22:12 +0100

    eau15.exs OK!

---
+++ eau15.exs
@@ -0,0 +1,8 @@
+end_of_sentence =
+  Enum.random([
+    "parfois compliqué d'adapter ma pensée à la programmation fonctionnelle",
+    "très formateur, ça m'a permis de comprendre la puissance de la programmation fonctionnelle",
+    "super interessant de jouer avec les defguard qui est un feature très partique"
+  ])
+
+IO.puts("j'ai terminé les épreuves de l'eau et c'etait #{end_of_sentence}")
