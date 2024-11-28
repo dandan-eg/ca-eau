6f69e2a42dfa2c5fa20fed03a4a75f8f101a0872
dandan-eg <egalledev@gmail.com>
2024-11-28 23:33:03 +0100

    ajout de la méthode run pour eau10.exs

--- eau10.exs
+++ eau10.exs
@@ -10,15 +10,19 @@ defmodule Exercice do
   defp do_find_index([_head | tail], target, index) do
     do_find_index(tail, target, index + 1)
   end
+
+  def run do
+    case System.argv() do
+      [] ->
+        IO.puts("usage elixir eau10.exs")
+
+      args ->
+        {word_to_find, rest_of_words} = List.pop_at(args, -1)
+
+        index = Exercice.find_index(rest_of_words, word_to_find)
+        IO.puts(index)
+    end
+  end
 end
 
-case System.argv() do
-  [] ->
-    IO.puts("usage elixir eau10.exs")
-
-  args ->
-    {word_to_find, rest_of_words} = List.pop_at(args, -1)
-
-    index = Exercice.find_index(rest_of_words, word_to_find)
-    IO.puts(index)
-end
+Exercice.run()
